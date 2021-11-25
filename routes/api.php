e73bba5663fabe290487ada820f72a80785c6b20
Noemi Ancari <noemiancari.na@gmail.com>
2021-11-25 11:18:53 -0400

    cambio de directorio de los controladores

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::group([
     'prefix' => 'pvt'
 ], function () {
     // Rutas abiertas
-    Route::post('/login', [App\Http\Controllers\Api\Auth\AuthController::class, 'login']);
+    Route::post('/login', [App\Http\Controllers\Auth\AuthController::class, 'login']);
     // Rutas autenticadas con token
     Route::group([
         'middleware' => ['auth:sanctum']
@@ -32,19 +32,19 @@ Route::group([
         Route::get('/profile', function(Request $request) {
             return auth()->user();
         });
-        Route::delete('/logout', [App\Http\Controllers\Api\Auth\AuthController::class, 'logout']);
-        Route::get('/auth', [App\Http\Controllers\Api\Auth\AuthController::class,'index']);
-        Route::patch('/refresh', [App\Http\Controllers\Api\Auth\AuthController::class, 'refresh']);
-        Route::apiResource('/role', App\Http\Controllers\Api\RoleController::class)->only(['index', 'show']);
-        Route::apiResource('/module', App\Http\Controllers\Api\ModuleController::class)->only(['index', 'show']);
-        Route::get('module/{module}/role', [App\Http\Controllers\Api\ModuleController::class, 'get_roles']);
-        Route::get('role/{role}/role_permisions', [App\Http\Controllers\Api\RoleController::class, 'role_permisions']);
-        Route::apiResource('/permission', App\Http\Controllers\Api\PermissionController::class)->only(['index']);
-        Route::patch('role/{role}/permission', [App\Http\Controllers\Api\RoleController::class,'set_or_remove_permission']);
-        Route::apiResource('/user', App\Http\Controllers\Api\UserController::class)->only(['index', 'store','show']);
-        Route::get('user/module_role_permision', [App\Http\Controllers\Api\UserController::class, 'module_role_permision']);
-        Route::get('user/{user}/module_role_state_user', [App\Http\Controllers\Api\UserController::class, 'module_role_state_user']);
-        Route::patch('user/{user}/role', [App\Http\Controllers\Api\UserController::class, 'set_or_remove_role']);
+        Route::delete('/logout', [App\Http\Controllers\Auth\AuthController::class, 'logout']);
+        Route::get('/auth', [App\Http\Controllers\Auth\AuthController::class,'index']);
+        Route::patch('/refresh', [App\Http\Controllers\Auth\AuthController::class, 'refresh']);
+        Route::apiResource('/role', App\Http\Controllers\Admin\RoleController::class)->only(['index', 'show']);
+        Route::apiResource('/module', App\Http\Controllers\Admin\ModuleController::class)->only(['index', 'show']);
+        Route::get('module/{module}/role', [App\Http\Controllers\Admin\ModuleController::class, 'get_roles']);
+        Route::get('role/{role}/role_permisions', [App\Http\Controllers\Admin\RoleController::class, 'role_permisions']);
+        Route::apiResource('/permission', App\Http\Controllers\Admin\PermissionController::class)->only(['index']);
+        Route::patch('role/{role}/permission', [App\Http\Controllers\Admin\RoleController::class,'set_or_remove_permission']);
+        Route::apiResource('/user', App\Http\Controllers\Admin\UserController::class)->only(['index', 'store','show']);
+        Route::get('user/module_role_permision', [App\Http\Controllers\Admin\UserController::class, 'module_role_permision']);
+        Route::get('user/{user}/module_role_state_user', [App\Http\Controllers\Admin\UserController::class, 'module_role_state_user']);
+        Route::patch('user/{user}/role', [App\Http\Controllers\Admin\UserController::class, 'set_or_remove_role']);
     });
 });
 

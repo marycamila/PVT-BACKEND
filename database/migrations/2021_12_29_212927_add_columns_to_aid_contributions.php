39fe77fc2c59ac447433864cf9b51dadb28b0536
Maritza <marycqmia@gmail.com>
2022-01-21 09:40:44 -0400

    agregado de columnas a la tabla aid_contributions

--- database/migrations/2021_12_29_212927_add_columns_to_aid_contributions.php
+++ database/migrations/2021_12_29_212927_add_columns_to_aid_contributions.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddContributionOriginIdToAidContributions extends Migration
+class AddColumnsToAidContributions extends Migration
 {
     /**
      * Run the migrations.
@@ -16,6 +16,7 @@ class AddContributionOriginIdToAidContributions extends Migration
         Schema::table('aid_contributions', function (Blueprint $table) {
             $table->foreign('contribution_origin_id')->references('id')->on('contribution_origins');
             $table->unsignedBigInteger('contribution_origin_id')->nullable(); // id del bien inmueble
+            $table->enum('affiliate_rent_class', ['VEJEZ', 'VIUDEDAD'])->default('VEJEZ')->comment('Tipo de Afiliado que Realizo el Aporte');        
         });
     }
 

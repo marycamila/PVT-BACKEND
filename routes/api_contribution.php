be5e9c395497a24aa6a32d53ce37e79ad961a7fd
Maritza <marycqmia@gmail.com>
2022-01-26 16:29:41 -0400

    PASO 3 Importación Senasir Métodos para el registro o Actualización de reportes

--- routes/api_contribution.php
+++ routes/api_contribution.php
@@ -23,6 +23,7 @@ Route::group([
         Route::get('/list_senasir_years', [App\Http\Controllers\Contribution\ImportPayrollSenasirController::class, 'list_senasir_years']);
         Route::post('/list_senasir_months', [App\Http\Controllers\Contribution\ImportPayrollSenasirController::class, 'list_senasir_months']);
         Route::post('/rollback_copy_validate_senasir', [App\Http\Controllers\Contribution\ImportPayrollSenasirController::class, 'rollback_copy_validate_senasir']);
+        Route::post('/import_create_or_update_contribution_payroll_period_senasir ', [App\Http\Controllers\Contribution\ImportPayrollSenasirController::class, 'import_create_or_update_contribution_payroll_period_senasir']);
     });
 });
 

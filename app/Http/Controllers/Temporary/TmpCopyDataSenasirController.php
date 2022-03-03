248853c6651adec3fa2c03c0200498ab2d69e29e
Maritza <marycqmia@gmail.com>
2022-03-03 14:24:26 -0400

    modificación del método upload_copy_affiliate_spouse_senasir para la ejecución en la base de datos auxiliar

--- app/Http/Controllers/Temporary/TmpCopyDataSenasirController.php
+++ app/Http/Controllers/Temporary/TmpCopyDataSenasirController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Helpers\Util;
 
 
 class TmpCopyDataSenasirController extends Controller
@@ -52,6 +53,9 @@ class TmpCopyDataSenasirController extends Controller
             $username = env('FTP_USERNAME');
             $password = env('FTP_PASSWORD');
             $file_name = 'afiliados_senasir'.'.'.$extension;
+            $db_connection_name_dblink = env("DB_CONNECTION_DBLINK");
+            //crear la conexion DBLINK a la base de datos principal
+            Util::open_connect_database_default();
             if($extension == "csv"){
             $base_path = 'afiliados';
             $file_path = Storage::disk('ftp')->putFileAs($base_path,$request->file,$file_name);
@@ -59,26 +63,26 @@ class TmpCopyDataSenasirController extends Controller
             $temporary_payroll = "create temporary table tmp_copy_data_senasir_aux(id_person_senasir integer,matricula_titular varchar, mat_dh varchar,
             carnet varchar, num_com varchar, paterno varchar, materno varchar, p_nombre varchar, s_nombre varchar, ap_casada varchar, fecha_nacimiento date, lugar_nacimiento varchar, clase_renta varchar,
             pat_titular varchar, mat_titular varchar, p_nom_titular varchar, s_nombre_titular varchar, ap_casada_titular varchar, carnet_tit varchar, num_com_tit varchar, fec_fail_tit date, lugar_nacimiento_tit varchar);";
-            $temporary_payroll = DB::select($temporary_payroll);
+            $temporary_payroll = DB::connection('db_aux')->select($temporary_payroll);
 
             $copy = "copy tmp_copy_data_senasir_aux(id_person_senasir ,matricula_titular , mat_dh ,
             carnet , num_com , paterno , materno , p_nombre , s_nombre , ap_casada , fecha_nacimiento , lugar_nacimiento , clase_renta ,
             pat_titular , mat_titular , p_nom_titular , s_nombre_titular , ap_casada_titular , carnet_tit , num_com_tit , fec_fail_tit , lugar_nacimiento_tit )
             FROM PROGRAM 'wget -q -O - $@  --user=$username --password=$password $base_path'
                     WITH DELIMITER ':' CSV header;";
-            $copy = DB::select($copy);
-
+            $copy = DB::connection('db_aux')->select($copy);
             $insert = "INSERT INTO tmp_copy_data_senasirs(id_person_senasir, matricula_titular, mat_dh,
-            carnet, num_com , concat_carnet_num_com, paterno, materno, p_nombre, s_nombre, ap_casada, fecha_nacimiento, lugar_nacimiento, clase_renta,
-            pat_titular, mat_titular, p_nom_titular, s_nombre_titular, ap_casada_titular, carnet_tit, num_com_tit, concat_carnet_num_com_tit, fec_fail_tit, lugar_nacimiento_tit,created_at)
-                       SELECT id_person_senasir, matricula_titular, mat_dh, carnet, num_com ,concat_identity_card_complement(carnet,num_com)::varchar as concat_carnet_num_com, paterno, materno, p_nombre, s_nombre, ap_casada, fecha_nacimiento, lugar_nacimiento, clase_renta,
-            pat_titular, mat_titular, p_nom_titular, s_nombre_titular, ap_casada_titular, carnet_tit, num_com_tit, concat_identity_card_complement(carnet_tit,num_com_tit)::varchar as concat_carnet_num_com_tit, fec_fail_tit, lugar_nacimiento_tit,current_timestamp  as created_at FROM  tmp_copy_data_senasir_aux; ";
-            $insert = DB::select($insert);
-            DB::commit();
+                        carnet, num_com , concat_carnet_num_com, paterno, materno, p_nombre, s_nombre, ap_casada, fecha_nacimiento, lugar_nacimiento, clase_renta,
+                        pat_titular, mat_titular, p_nom_titular, s_nombre_titular, ap_casada_titular, carnet_tit, num_com_tit, concat_carnet_num_com_tit, fec_fail_tit, lugar_nacimiento_tit,created_at)
+                                   SELECT id_person_senasir, matricula_titular, mat_dh, carnet, num_com ,(select * from dblink('$db_connection_name_dblink','SELECT * FROM concat_identity_card_complement('''||carnet||''','''||carnet||''')'::text) as  uu(concat_carnet_num_com character varying(250))) as concat_carnet_num_com, paterno, materno, p_nombre, s_nombre, ap_casada, fecha_nacimiento, lugar_nacimiento, clase_renta,
+                        pat_titular, mat_titular, p_nom_titular, s_nombre_titular, ap_casada_titular, carnet_tit, num_com_tit,(select * from dblink('$db_connection_name_dblink','SELECT * FROM concat_identity_card_complement('''||carnet_tit||''','''||num_com_tit||''')'::text) as  uu(concat_carnet_num_com character varying(250)))  as concat_carnet_num_com_tit, fec_fail_tit, lugar_nacimiento_tit,current_timestamp  as created_at FROM  tmp_copy_data_senasir_aux;";
+            $insert = DB::connection('db_aux')->select($insert);
             $drop = "drop table if exists tmp_copy_data_senasir_aux";
             $drop = DB::select($drop);
+            Util::close_conection_database_default();
+            DB::commit();
             $consult = "select  count(*) from tmp_copy_data_senasirs";
-                        $consult = DB::select($consult)[0]->count;
+                        $consult = DB::connection('db_aux')->select($consult)[0]->count;
                         return response()->json([
                             'message' => 'Realizado con exito',
                             'payload' => [

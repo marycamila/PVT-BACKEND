f17919b16381a4222a5428b69b7c2819240c1280
Maritza <marycqmia@gmail.com>
2022-03-10 17:15:20 -0400

    refactorización de funciones de importacion de contribuciones senasir

--- database/migrations/2022_02_09_154306_create_function_update_or_create_spouse_and_update_affiliate.php
+++ database/migrations/2022_02_09_154306_create_function_update_or_create_spouse_and_update_affiliate.php
@@ -13,7 +13,7 @@ class CreateFunctionUpdateOrCreateSpouseAndUpdateAffiliate extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE OR REPLACE FUNCTION public.update_or_create_spouse_and_update_affiliate(affiliate bigint, user_reg integer,aid_contribution_affiliate_payroll_senasir_id integer)
+        DB::statement("CREATE OR REPLACE FUNCTION public.update_or_create_spouse_and_update_affiliate(affiliate bigint, user_reg integer, payroll_validated_senasir_id integer)
         returns varchar
         as $$
         declare
@@ -21,42 +21,42 @@ class CreateFunctionUpdateOrCreateSpouseAndUpdateAffiliate extends Migration
         id_pension_entity int;
         id_affiliate_state int;
         begin
-            --************************************************************************************
-            --Funcion par crear o actualizar datos de la esposa y actualizar algunos datos del afiliado
-            --************************************************************************************
-            id_pension_entity:=  (SELECT id FROM pension_entities WHERE name ='SENASIR') as id;
-            id_affiliate_state:=  (SELECT id FROM affiliate_states WHERE name ='Fallecido') as id;
-            if exists(SELECT  * FROM aid_contribution_affiliate_payroll_senasirs acaps WHERE id = aid_contribution_affiliate_payroll_senasir_id and clase_renta='VIUDEDAD' and fec_fail_tit is not null and affiliate_id= affiliate) then
+            --*******************************************************************************************
+            --Funcion par crear o actualizar datos de la esposa y actualizar algunos datos del afiliado--
+            --*******************************************************************************************
+            id_pension_entity:=  (SELECT id FROM pension_entities WHERE name ='SENASIR');
+            id_affiliate_state:=  (SELECT id FROM affiliate_states WHERE name ='Fallecido');
+            if exists(SELECT  * FROM payroll_validated_senasirs pvs WHERE pvs.id = payroll_validated_senasir_id and pvs.clase_renta='VIUDEDAD' and fec_fail_tit is not null and affiliate_id= affiliate) then
                 if exists(SELECT * FROM spouses WHERE affiliate_id = affiliate) then
                  message:= 'actualiza esposa';
                       UPDATE public.spouses
                       SET user_id = user_reg,
-                      registration = acaps.mat_dh,
+                      registration = pvs.mat_dh,
                       updated_at = (select current_timestamp)
-                      FROM (SELECT * FROM aid_contribution_affiliate_payroll_senasirs WHERE id = aid_contribution_affiliate_payroll_senasir_id) AS acaps
-                      WHERE spouses.affiliate_id = affiliate and  (spouses.registration = '' or spouses.registration is null);
+                      FROM (SELECT * FROM payroll_validated_senasirs WHERE id = payroll_validated_senasir_id) AS pvs
+                      WHERE spouses.affiliate_id = 8480 and (spouses.registration in ('','0') or spouses.registration is null);      
 
                       UPDATE public.spouses
                       SET user_id = user_reg,
-                      birth_date = acaps.fecha_nacimiento,
+                      birth_date = pvs.fecha_nacimiento,
                       updated_at = (select current_timestamp)
-                      FROM (SELECT * FROM aid_contribution_affiliate_payroll_senasirs WHERE id = aid_contribution_affiliate_payroll_senasir_id) AS acaps
-                      WHERE spouses.affiliate_id = affiliate and (spouses.registration = '' or spouses.birth_date is null);
+                      FROM (SELECT * FROM payroll_validated_senasirs WHERE id = payroll_validated_senasir_id) AS pvs
+                      WHERE spouses.affiliate_id = affiliate and (spouses.birth_date = '' or spouses.birth_date is null);
 
                 else
                 message:=  'crear esposa';
                       INSERT INTO public.spouses(user_id, affiliate_id,identity_card,registration, last_name, mothers_last_name , first_name , second_name, created_at,updated_at, birth_date)
-                      SELECT user_reg as user_id, acasps.affiliate_id, acasps.carnet_num_com as identity_card, acasps.mat_dh as registration, acasps.paterno as last_name, acasps.materno as mothers_last_name,acasps.p_nombre as first_name, acasps.s_nombre as second_name,(select current_timestamp as created_at),(select current_timestamp as updated_at), acasps.fecha_nacimiento as birth_date
-                      FROM aid_contribution_affiliate_payroll_senasirs acasps
-                      WHERE id=aid_contribution_affiliate_payroll_senasir_id;
+                      SELECT user_reg as user_id, pvs.affiliate_id, pvs.carnet_num_com as identity_card, pvs.mat_dh as registration, pvs.paterno as last_name, pvs.materno as mothers_last_name,pvs.p_nombre as first_name, pvs.s_nombre as second_name,(select current_timestamp as created_at),(select current_timestamp as updated_at), pvs.fecha_nacimiento as birth_date
+                      FROM payroll_validated_senasirs pvs
+                      WHERE id=payroll_validated_senasir_id;
                 end if;
                message:= 'Se actualizar afiliado y '||message;
 
                UPDATE public.affiliates
                SET user_id = user_reg,
-               date_death = acaps.fec_fail_tit,
+               date_death = pvs.fec_fail_tit,
                updated_at = (select current_timestamp)
-               FROM (SELECT * FROM aid_contribution_affiliate_payroll_senasirs WHERE id = aid_contribution_affiliate_payroll_senasir_id) AS acaps
+               FROM (SELECT * FROM payroll_validated_senasirs WHERE id = payroll_validated_senasir_id) AS pvs
                WHERE affiliates.id = affiliate and (affiliates.date_death = '' or affiliates.date_death is null);
 
                UPDATE public.affiliates
@@ -69,8 +69,8 @@ class CreateFunctionUpdateOrCreateSpouseAndUpdateAffiliate extends Migration
             message:=  'Se actualiza afiliado';
                UPDATE public.affiliates
                SET user_id = user_reg,
-               birth_date = acaps.fecha_nacimiento, updated_at = (select current_timestamp)
-               FROM (SELECT * FROM aid_contribution_affiliate_payroll_senasirs WHERE id = aid_contribution_affiliate_payroll_senasir_id) AS acaps
+               birth_date = pvs.fecha_nacimiento, updated_at = (select current_timestamp)
+               FROM (SELECT * FROM payroll_validated_senasirs WHERE id = payroll_validated_senasir_id) AS pvs
                WHERE affiliates.id = affiliate and (affiliates.birth_date = '' or affiliates.birth_date is null);
 
            end if;
@@ -93,6 +93,6 @@ class CreateFunctionUpdateOrCreateSpouseAndUpdateAffiliate extends Migration
      */
     public function down()
     {
-        DB::statement("DROP FUNCTION update_or_create_spouse_and_update_affiliate");
+       DB::statement("DROP FUNCTION update_or_create_spouse_and_update_affiliate");
     }
 }

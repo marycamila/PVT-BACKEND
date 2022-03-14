d2334e662b1220e365ae82ebef4a894d23469a3b
jiuseppe <jiuseppeflores@gmail.com>
2022-03-14 13:52:18 -0400

    Creacion Modelo PayrollValidatedSenasir

---
+++ app/Models/Contribution/PayrollValidatedSenasir.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models\Contribution;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PayrollValidatedSenasir extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    public $guarded = ['id'];
+    protected $fillable = [
+        'affiliate_id',
+        'a_o',
+        'mes',
+        'id_person_titular',
+        'matricula_titular',
+        'mat_dh',
+        'departamento',
+        'regional',
+        'renta',
+        'tipo_renta',
+        'carnet_num_com',
+        'paterno',
+        'materno',
+        'p_nombre',
+        's_nombre',
+        'ap_casada',
+        'fecha_nacimiento',
+        'clase_renta',
+        'total_ganado',
+        'total_descuentos',
+        'liquido_pagable',
+        'rentegro_r_basica',
+        'renta_dignidad',
+        'reintegro_renta_dignidad',
+        'reintegro_aguinaldo',
+        'reintegro_importe_adicional',
+        'reintegro_inc_gestion',
+        'descuento_aporte_muserpol',
+        'descuento_covipol',
+        'descuento_prestamo_muserpol',
+        'carnet_num_com_tit',
+        'pat_titular',
+        'mat_titular',
+        'p_nom_titular',
+        's_nombre_titular',
+        'ap_casada_titular',
+        'fecha_nac_titular',
+        'clase_renta_tit',
+        'fec_fail_tit',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}

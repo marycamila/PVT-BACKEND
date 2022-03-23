48f1104cce0479543c1d5aa575093b71bb9a8ec1
Maritza <marycqmia@gmail.com>
2022-03-23 18:09:24 -0400

    Creacion de la tabla             Schema::create('contribution_passives', function (Blueprint ) {

---
+++ database/migrations/2021_12_29_212927_create_contribution_passives_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContributionPassivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            Schema::create('contribution_passives', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');  // id usuario
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('affiliate_id')->unsigned();
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+            $table->date('month_year')->comment('Periodo del aporte');
+            $table->unique(['affiliate_id', 'month_year']);
+            $table->decimal('quotable',13,2)->default(0)->comment('cotizable');
+            $table->decimal('rent',13,2)->default(0)->comment('Renta');
+            $table->decimal('dignity_rent',13,2)->default(0)->comment('Renta dignidad');
+            $table->decimal('interest',13,2)->default(0)->comment('Interes');
+            $table->decimal('total',13,2)->default(0)->comment('Total aporte');
+            $table->boolean('is_valid')->default(false)->comment('Si el aporte es valido');
+            $table->enum('affiliate_rent_class', ['VEJEZ', 'VIUDEDAD'])->default('VEJEZ')->comment('Tipo de Afiliado que realizo el Aporte');
+            $table->nullableMorphs('contributionable'); // Campo para contribuiciones de aportes directos y complemento economico
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contribution_passives');
+    }
+}

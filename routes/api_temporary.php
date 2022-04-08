a332e6bf871edd761778ee7dc21a26882bc7cd3e
Maritza <marycqmia@gmail.com>
2022-04-08 13:56:57 -0400

    metodo para actualizar ids de afiliado por criterio y contero de registros por criterio

--- routes/api_temporary.php
+++ routes/api_temporary.php
@@ -15,6 +15,7 @@ Route::group([
         Route::post('upload_copy_person_senasir', [App\Http\Controllers\Temporary\CopyPersonSenasirController::class, 'upload_copy_person_senasir']);
         Route::post('update_affiliate_id_person_senasir', [App\Http\Controllers\Temporary\CopyPersonSenasirController::class, 'update_affiliate_id_person_senasir']);
         Route::post('update_person_senasir_id', [App\Http\Controllers\Temporary\CopyPersonSenasirController::class, 'update_person_senasir_id']);
+        Route::post('update_affiliate_id_senasir', [App\Http\Controllers\Temporary\CopyPersonSenasirController::class, 'update_affiliate_id_senasir']);
     });
 });
 

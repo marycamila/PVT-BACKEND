be54f4967894abd079ccf616ebd2341185b3200d
bdquispe <daysi.quispe444@gmail.com>
2022-05-03 08:30:12 -0400

    creación de modelos procedure y workflow

--- app/Models/Contribution/ContributionProcess.php
+++ app/Models/Contribution/ContributionProcess.php
@@ -5,6 +5,9 @@ namespace App\Models\Contribution;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Admin\User;
+use App\Models\Procedure\ProcedureState;
+use App\Models\Workflow\Workflow;
+use App\Models\Workflow\WfState;
 
 class ContributionProcess extends Model
 {
@@ -27,5 +30,17 @@ class ContributionProcess extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function procedure_state()
+    {
+      return $this->belongsTo(ProcedureState::class); 
+    }
+    public function workflow()
+    {
+      return $this->belongsTo(Workflow::class); 
+    }
+    public function wf_state()
+    {
+        return $this->belongsTo(WfState::class,'wf_state_current_id','id');
     }
 }

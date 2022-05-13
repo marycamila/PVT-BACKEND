80b8f1f9fc1b62508a077f78c88bdb5f6def87e5
zeratulwin <juanjose2101@gmail.com>
2022-05-13 12:34:02 -0400

    Creación de la tabla import_records

--- database/migrations/2022_05_13_140226_create_import_records_table.php
+++ database/migrations/2022_05_13_140226_create_import_records_table.php
@@ -17,17 +17,14 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->unsigned()->comment('Id del usuario');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('payroll_commands_id')->unsigned()->comment('Id planilla Comando');
-            $table->foreign('payroll_commands_id')->references('id')->on('payroll_commands');
-            $table->unsignedBigInteger('payroll_senasirs_id')->unsigned()->comment('Id planilla SENASIR');
-            $table->foreign('payroll_senasirs_id')->references('id')->on('payroll_senasirs');
+            $table->morphs('import_recordable');
             $table->unsignedBigInteger('record_types_id')->unsigned();
             $table->foreign('record_types_id')->references('id')->on('record_types');
             $table->unsignedBigInteger('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles');
             $table->string('action')->comment('Descripción de la acción');
             $table->timestamps();
-            $table->softDeletes();  
+            $table->softDeletes();
         });
     }
 

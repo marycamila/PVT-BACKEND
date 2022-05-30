76d36ca46ddcbc57e0aa41b308af59cee5d21955
Noemi Ancari <noemiancari.na@gmail.com>
2022-05-29 22:30:33 -0400

    prueba token

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Auth\AuthenticationException;
 
+use App\Http\Controllers\AuthController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
 Route::group([
     'middleware' => 'api',
 ], function () {

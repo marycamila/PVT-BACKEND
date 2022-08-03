45e43f671f5cb9058f18efa7fe24c248a8b6f012
MaxLyonel <leorexx100@gmail.com>
2022-08-03 15:42:45 -0400

    Implementación de una base de datos

---
+++ app/Models/Notification/NotificationSend.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Notification;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Notification\NotificationNumber;
+use App\Models\Notification\NotificationCarrier;
+use App\Models\Admin\User;
+
+class NotificationSend extends Model
+{
+    use HasFactory;
+
+    public function number(){
+        return $this->belongsTo(NotificationNumber::class);
+    }
+
+    public function carrier(){
+        return $this->belongsTo(NotificationCarrier::class);
+    }
+    
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}

3d37f4143e2bf6544a16cad347cb414d4708708f
Maritza <marycqmia@gmail.com>
2023-03-23 15:11:24 -0400

    Cambios en la creacion de la tabla payroll_copy_transcripts

--- database/migrations/2023_03_23_144211_create_payroll_copy_transcripts_table.php
+++ database/migrations/2023_03_23_144211_create_payroll_copy_transcripts_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
     {
         Schema::connection('db_aux')->create('payroll_copy_transcripts', function (Blueprint $table) {
             $table->id();
-            $table->integer('num')->comment('Numero de registro');
-            $table->string('obs')->comment('observacion');
-            $table->string('uni')->comment('Unidad');
+            $table->string('obs')->nullable()->comment('observacion');
+            $table->string('uni')->default(null)->comment('Unidad');
             $table->integer('mes')->comment('Mes');
             $table->integer('a_o')->comment('Año');
             $table->string('car')->comment('Carnet');
@@ -25,19 +24,19 @@ return new class extends Migration
             $table->string('mat')->nullable()->comment('Apellido materno');
             $table->string('nom')->nullable()->comment('Primer nombre');
             $table->string('nom2')->nullable()->comment('Segundo nombre');
-            $table->decimal('gan', 13, 2)->comment('Total ganado');
-            $table->decimal('mus', 13, 2)->comment('Aporte Muserpol');
-            $table->string('niv')->comment('Nivel jerarquico');
-            $table->string('gra')->comment('Grado');
-            $table->decimal('sue', 13, 2)->comment('Sueldo');
-            $table->decimal('cat', 13, 2)->comment('Bono antiguedad');
-            $table->decimal('est', 13, 2)->comment('Bono estudio');
-            $table->decimal('carg', 13, 2)->comment('Bono cargo');
-            $table->decimal('fro', 13, 2)->comment('Bono frontera');
-            $table->decimal('ori', 13, 2)->comment('Bono oriente');
+            $table->decimal('gan', 13, 2)->default(0)->comment('Total ganado');
+            $table->decimal('mus', 13, 2)->default(0)->comment('Aporte Muserpol');
+            $table->string('niv')->default(null)->comment('Nivel jerarquico');
+            $table->string('gra')->default(null)->comment('Grado');
+            $table->decimal('sue', 13, 2)->default(0)->comment('Sueldo');
+            $table->decimal('cat', 13, 2)->default(0)->comment('Bono antiguedad');
+            $table->decimal('est', 13, 2)->default(0)->comment('Bono estudio');
+            $table->decimal('carg', 13, 2)->default(0)->comment('Bono cargo');
+            $table->decimal('fro', 13, 2)->default(0)->comment('Bono frontera');
+            $table->decimal('ori', 13, 2)->default(0)->comment('Bono oriente');
             $table->date('nac')->nullable()->comment('Fecha de nacimiento');
             $table->date('ing')->nullable()->comment('Fecha de ingreso');
-            $table->unsignedBigInteger('affiliate_id')->unsigned()->nullable()->comment('Id del afiliado titular');
+            $table->unsignedBigInteger('affiliate_id')->nullable()->comment('Id del afiliado titular');
             $table->enum('state', ['accomplished','unrealized'])->default('unrealized')->comment('Estado si fue encontrado o no encontrado');
             $table->string('criteria')->nullable()->comment('creitetio de identificacion del afiliado');
             $table->timestamps();
